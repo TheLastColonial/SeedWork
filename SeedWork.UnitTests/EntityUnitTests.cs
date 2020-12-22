aab7dca08677b802d68ecad21d52c5a05b4d991e
TheLastColonial <alex.aim88@gmail.com>
2020-12-22 20:08:29 +0000

    Added Entity with simple UT

---
+++ SeedWork.UnitTests/EntityUnitTests.cs
@@ -0,0 +1,26 @@
+﻿namespace SeedWork.UnitTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class EntityUnitTests
+    {
+        [Test]
+        public void GivenAnEntityHasAPrimaryKey_WhenContructionWithInt_ThenEntitySuccessfulCreates()
+        {
+            // Arrange
+
+            // Act
+            var entity = new Entity<int>(1);
+
+            // Assert
+            entity.Should().NotBeNull()
+                .And.BeOfType(typeof(Entity<int>));
+            entity.Id.Should().Be(1);
+        }
+    }
+}
---
+++ SeedWork/Entity.cs
@@ -0,0 +1,27 @@
+﻿namespace SeedWork
+{
+    using System;
+
+    /// <summary>
+    /// Entity within a bounded context
+    /// </summary>
+    /// <typeparam name="T">Id Type</typeparam>
+    /// <seealso cref="https://docs.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/microservice-domain-model#the-domain-entity-pattern"/>
+    public class Entity<T>
+    {
+        /// <summary>
+        /// Key
+        /// </summary>
+        public T Id { get; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="id">Primary key of the <see cref="Entity{T}"/></param>
+        public Entity(T id)
+        {
+            this.Id = id ?? throw new ArgumentNullException(nameof(id));
+        }
+
+
+    }
+}
