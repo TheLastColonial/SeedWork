aadf0d0e7527803d01a9fc4a5f824e402d622600
TheLastColonial <alex.aim88@gmail.com>
2021-03-12 19:06:20 +0000

    Added ValueObject

---
+++ SeedWork/ValueObject.cs
@@ -0,0 +1,54 @@
+﻿namespace SeedWork
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A collection of data that relate to each other but do not have an identifier.
+    /// </summary>
+    /// <seealso cref="https://martinfowler.com/bliki/ValueObject.html"/>
+    /// <seealso cref="https://docs.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/implement-value-objects"/>
+    public abstract class ValueObject
+    {
+        protected static bool EqualOperator(ValueObject left, ValueObject right)
+        {
+            if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+            {
+                return false;
+            }
+
+            return ReferenceEquals(left, null) || left.Equals(right);
+        }
+
+        protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+        {
+            return !(EqualOperator(left, right));
+        }
+
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var other = (ValueObject)obj;
+
+            return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+             .Select(x => x != null ? x.GetHashCode() : 0)
+             .Aggregate((x, y) => x ^ y);
+        }
+
+        public ValueObject GetCopy()
+        {
+            return this.MemberwiseClone() as ValueObject;
+        }
+    }
+}
