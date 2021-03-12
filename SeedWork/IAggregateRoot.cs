9e4a22f99baf9ac56eaf2597fa0f8fb71cd2d21c
TheLastColonial <alex.aim88@gmail.com>
2021-03-12 19:05:53 +0000

    Added IAggregateRoot

---
+++ SeedWork/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace SeedWork
+{
+    /// <summary>
+    /// Flag interface for identifing aggregate root objects
+    /// </summary>
+    /// <seealso cref="https://martinfowler.com/bliki/DDD_Aggregate.html"/>
+    /// <seealso cref="https://stackoverflow.com/questions/1958621/whats-an-aggregate-root#:~:text=Aggregate%20Root%20is%20the%20mothership,Entry%2DPoint%20to%20an%20Aggregate."/>
+    public interface IAggregateRoot
+    {
+    }
+}
